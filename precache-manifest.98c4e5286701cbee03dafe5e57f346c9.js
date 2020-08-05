866943b8cc7f3096f40075a45b870ac60bb718f5
prakhar <prakhar.chhipa@airtel.com>
2020-08-06 01:18:55 +0530

    Updates

---
+++ precache-manifest.98c4e5286701cbee03dafe5e57f346c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4403f1f0ffdb91279dfb454e0ac3b317",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5944cd938b4136dd2957",
+    "url": "/static/css/main.56faaeff.chunk.css"
+  },
+  {
+    "revision": "27152e9623811b3d0ecd",
+    "url": "/static/js/2.0c7cf1fd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.0c7cf1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5944cd938b4136dd2957",
+    "url": "/static/js/main.82689131.chunk.js"
+  },
+  {
+    "revision": "2ad06e95052a35acf89b",
+    "url": "/static/js/runtime-main.27064ad5.js"
+  }
+]);
